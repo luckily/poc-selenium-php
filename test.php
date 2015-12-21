228690f19fce9fe040e9dfbb892c59c445f02cca
joel.zhong <luckily.zhong@gmail.com>
2015-12-22 04:38:26 +0800

    first commit

---
+++ test.php
@@ -0,0 +1,45 @@
+<?php
+
+require './vendor/autoload.php';
+
+class WebTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+
+    public static function browsers()
+    {
+        return array(
+            // array(
+            //      'host' => '127.0.0.1',
+            //      'port' => 4444,
+            //      'browser' => 'firefox test browser',
+            //      'browserName' => 'firefox',
+            // ),
+            array(
+                 'host' => '127.0.0.1',
+                 'port' => 4444,
+                 'browser' => 'chrome test browser',
+                 'browserName' => 'chrome',
+            ),
+        );
+    }
+
+    protected function setUp()
+    {
+        $this->setBrowserUrl('http://www.104.com.tw/');
+    }
+
+
+    public function setUpPage()
+    {
+        $this->url('/');
+    }
+
+    public function testTitle()
+    {
+        $element = $this->byCssSelector('.logo');
+        var_dump($element->attribute('title'));
+        $this->assertEquals($element->attribute('title'), '104人力銀行 - 不只找工作為你找方向');
+        sleep(10);
+    }
+}
+
